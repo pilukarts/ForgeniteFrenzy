9852174404d8c5008dd0534644773d7728b308a2
Maria del Pilar Rodriguez Castillejo <pilarrodriguezcastillejo@gmail.com>
2025-06-14 21:08:37 +0000

    oh   tiene marca de agua

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function HomePage() {
   }
   
   const backgroundImageStyle = {
-    backgroundImage: "url('https://i.imgur.com/ho2vdvv.png')", 
+    backgroundImage: "url('https://placehold.co/1920x1080.png')", 
   };
 
   return (
@@ -37,7 +37,7 @@ export default function HomePage() {
       <div 
         className="flex flex-col items-center justify-center text-center h-full pt-2 pb-16 bg-cover bg-center bg-no-repeat"
         style={backgroundImageStyle}
-        data-ai-hint="abstract background" 
+        data-ai-hint="abstract background space" 
       >
         <CommanderPortrait 
           commanderSex={playerProfile.commanderSex} 
@@ -88,4 +88,3 @@ export default function HomePage() {
     </AppLayout>
   );
 }
-
