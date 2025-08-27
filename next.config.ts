8d5a669e3d622377cf5207e5daa04347b4b9dc3c
Maria del Pilar Rodriguez Castillejo <pilarrodriguezcastillejo@gmail.com>
2025-08-27 15:58:52 +0000

    thspec 'describe the error in the   new  commit' did not match any file(

--- next.config.ts
+++ next.config.ts
@@ -25,11 +25,6 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  devServer: {
-    allowedDevOrigins: [
-        "https://*.cloudworkstations.dev",
-    ]
-  }
 };
 
 export default nextConfig;
