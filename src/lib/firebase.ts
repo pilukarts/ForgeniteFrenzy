f81000af3c2ca5dce8877afeea437bc3063ee253
Maria del Pilar Rodriguez Castillejo <pilarrodriguezcastillejo@gmail.com>
2025-09-01 19:35:42 +0000

    4 Agrega el SDK de Firebase
    Usar npm
    
    Usar una etiqueta <script>
    Si ya

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMdkst7F612rqEyu0iF86CaYOdJGBXPaw",
+  authDomain: "ark-evac.firebaseapp.com",
+  projectId: "ark-evac",
+  storageBucket: "ark-evac.appspot.com",
+  messagingSenderId: "928400685620",
+  appId: "1:928400685620:web:a6c60bd47e47d4ae9b6861"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app };
