32bf06be5bc991b6c70a94a7bf6a9276f3c615b9
Maria del Pilar Rodriguez Castillejo <pilarrodriguezcastillejo@gmail.com>
2025-09-02 01:05:06 +0000

    en  la app solose  ve  hasta el nivel 31

--- src/components/game/LevelMap.tsx
+++ src/components/game/LevelMap.tsx
@@ -11,7 +11,7 @@ import { useToast } from '@/hooks/use-toast';
 import { LEVEL_STAGES } from '@/lib/gameData';
 
 const LEVELS_BEFORE = 10; // How many past levels to show
-const LEVELS_AFTER = 30; // How many upcoming levels to show
+const LEVELS_AFTER = 100; // How many upcoming levels to show
 const TOTAL_LEVELS = 50000; // The total number of levels in this stage
 
 const LevelMap: React.FC = () => {
