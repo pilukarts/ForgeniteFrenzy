278fa07da8928efa3eb00470dd0c989f181a2b96
Maria del Pilar Rodriguez Castillejo <pilarrodriguezcastillejo@gmail.com>
2025-09-06 13:26:44 +0000

    no pundiona el  despliegue de naciones en  mi telgram ap del pc

--- src/components/player/PlayerSetup.tsx
+++ src/components/player/PlayerSetup.tsx
@@ -106,7 +106,7 @@ const PlayerSetup: React.FC = () => {
                         <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                       </Button>
                     </PopoverTrigger>
-                    <PopoverContent className="w-[--radix-popover-trigger-width] p-0">
+                    <PopoverContent className="w-[--radix-popover-trigger-width] p-0 z-[200]">
                       <Command>
                         <CommandInput 
                           placeholder="Search nation..." 
