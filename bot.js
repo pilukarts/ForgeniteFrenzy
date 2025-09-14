07966da227546e578a62eaaee7182be3accca96e
Maria del Pilar Rodriguez Castillejo <pilarrodriguezcastillejo@gmail.com>
2025-09-14 04:26:43 +0000

    tenemos que anadir que tenemos ya un dominio   forgeitedrenzy.online

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var telegraf_1 = require("telegraf");
 var TOKEN = '8275463245:AAEG-X1D8Y-xtppY_7WVJobn488WMxkYLEw';
-var GAME_URL = 'https://nextn.web.app';
+var GAME_URL = 'https://forgeitedrenzy.online';
 var bot = new telegraf_1.Telegraf(TOKEN);
 bot.start(function (ctx) {
     ctx.reply('Welcome to Forgeite Frenzy!\nType /play to launch the game or /help for more info.');
