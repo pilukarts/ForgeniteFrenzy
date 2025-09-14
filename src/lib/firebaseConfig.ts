0ae76d7fbb00e026e95b1aac15908ad13833befe
Maria del Pilar Rodriguez Castillejo <pilarrodriguezcastillejo@gmail.com>
2025-09-14 18:16:38 +0000

    Try fixing this error: `Console Error: {} {} Error: WebSocket connection

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -1,9 +1,10 @@
+
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyDMdkst7F612rqEyu0iF86CaYOdJGBXPaw",
-  authDomain: "forgeite-frenzy.firebaseapp.com",
-  projectId: "forgeite-frenzy",
-  storageBucket: "forgeite-frenzy.appspot.com",
+  apiKey: "AIzaSyA2k6Fj-v5r3pA5Z3Z8i-v8Y8d7c5s4X3E",
+  authDomain: "genkit-90196.firebaseapp.com",
+  projectId: "genkit-90196",
+  storageBucket: "genkit-90196.appspot.com",
   messagingSenderId: "928400685620",
-  appId: "1:928400685620:web:630f671a2fdaa4449b6861"
+  appId: "1:928400685620:web:48c3b537a723825a9b6861"
 };
