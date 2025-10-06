1a580ead0a6bf7c3c0e2c3ba7e21d03477c66272
Maria del Pilar Rodriguez Castillejo <pilarrodriguezcastillejo@gmail.com>
2025-10-06 01:30:17 +0000

    sigue sin aparecer  el ark debajo tiene su countdown  y   el fondo de l

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -104,7 +104,7 @@ const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
     <div 
       className="flex flex-col min-h-screen text-foreground items-center justify-center"
       style={{
-        backgroundImage: `url('${images.global.mainScreenBackground}')`,
+        backgroundImage: `url('${images.global.main_scene}')`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         animation: 'pan-background-global 90s linear infinite',
